c8b4926521b47f37eb1310664261bf9a00d4e830
Dalena <mimidalena@gmail.com>
2022-06-13 11:08:34 -0700

    README

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { IThemeManager } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the ape_theme extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'ape_theme:plugin',
+  autoStart: true,
+  requires: [IThemeManager],
+  activate: (app: JupyterFrontEnd, manager: IThemeManager) => {
+    console.log('JupyterLab extension ape_theme is activated!');
+    const style = 'ape_theme/index.css';
+
+    manager.register({
+      name: 'ape_theme',
+      isLight: true,
+      load: () => manager.loadCSS(style),
+      unload: () => Promise.resolve(undefined)
+    });
+  }
+};
+
+export default plugin;
